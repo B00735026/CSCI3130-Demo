e782e90652c5abd9e5458323d30bbc05e114e499
B00735026 <Albertmeng520@gmail.com>
2018-05-19 21:06:21 -0300

    This is assignment1 for 3130 Add three new item in this demo: Button, TextView and EditText.Then, connect them together.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+     String textview1;
+     Button btn;
+     EditText tx;
+     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// get the button
+        btn = (Button) findViewById(R.id.btnDosomething);
+        tx = (EditText) findViewById(R.id.editText);
+        tv = (TextView)findViewById(R.id.textView);
+        //set what happens when user clicks
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String str = tx.getText().toString();
+                tv.setText(str);
+            }
+        });
     }
 }
